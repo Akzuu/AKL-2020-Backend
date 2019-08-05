3786332f077d38898c0da03ac392bb1be8fef085
Akseli Kolari <akzuu@iname.com>
2019-08-05 18:15:18 +0300

    init

---
+++ database/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const { log } = require('../lib');
+
+const MONGO_URI = config.get('database.mongo.uri');
+const MONGO_OPTIONS = config.get('datbase.mongo.options');
+
+const startDatabase = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, MONGO_OPTIONS);
+  } catch (error) {
+    log.error('Error connecting mongodb!', error);
+  }
+};
+
+startDatabase();
