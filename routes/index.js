b74e76ad73b133cc64112c972bb837082c223025
Akseli Kolari <akzuu@iname.com>
2019-09-18 17:01:00 +0300

    Making sure the backend can be ran in local environment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const utility = require('./utility');
 const user = require('./user');
+const team = require('./team');
+const season = require('./season');
+const integration = require('./integration');
 
 module.exports = {
   utility,
   user,
+  team,
+  season,
+  integration,
 };
