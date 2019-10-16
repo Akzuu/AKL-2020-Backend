2baa299c5908424a61c35b6d8fa5185877520581
Akseli Kolari <akseli.kolari@tuni.fi>
2019-10-16 09:36:00 +0300

    Creating user now creates token and returns it to browser

--- routes/user/create-user.js
+++ routes/user/create-user.js
@@ -14,24 +14,51 @@ const schema = {
         status: {
           type: 'string',
         },
+        token: {
+          type: 'string',
+        },
       },
     },
   },
 };
 
-const preHandler = (req, reply, done) => {
-  done();
-};
-
 const handler = async (req, reply) => {
+  // Generate JWT token
+  let token;
   try {
-    await User.create(req.body);
+    token = await reply.jwtSign({ userName: req.body.userName });
   } catch (error) {
-    log.error('Error when trying to create user! ', error);
-    reply.status(500).send();
+    log.error('Error creating token!', error);
   }
 
-  reply.send({ status: 'OK' });
+  // Pass token to body, so it will be saved to database
+  if (token) {
+    req.body.token = token;
+  }
+
+  let user;
+  try {
+    user = await User.create(req.body);
+  } catch (error) {
+    log.error('Error when trying to create user! ', { error, body: req.body });
+    reply.status(500).send({
+      status: 'ERROR',
+      error: 'Internal Server Error',
+    });
+    return;
+  }
+
+  if (!user) {
+    log.error('Make sure the given information is correct!', req.body);
+    reply.status(400).send({
+      status: 'ERROR',
+      error: 'Bad Request',
+      message: 'Make sure the given information is correct!',
+    });
+    return;
+  }
+
+  reply.send({ status: 'OK', token });
 };
 
 module.exports = {
@@ -39,5 +66,4 @@ module.exports = {
   url: '/create',
   schema,
   handler,
-  preHandler,
 };
