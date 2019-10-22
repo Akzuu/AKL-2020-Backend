3ab43b3e0f075e369b33bbc9a45e07c349c4ac70
Akseli Kolari <akseli.kolari@tuni.fi>
2019-10-22 12:20:43 +0300

    Ranks

--- models/team.js
+++ models/team.js
@@ -35,6 +35,15 @@ const schema = new Schema({
     type: Boolean,
     required: true,
   },
+  rank: {
+    type: String,
+    enum: [
+      'Silver I', 'Silver II', 'Silver III', 'Silver IV', 'Silver Elite', 'Silver Elite Master',
+      'Gold Nova I', 'Gold Nova II', 'Gold Nova III', 'Gold Nova Master',
+      'Master Guardian I', 'Master Guardian II', 'Master Guardian Elite',
+      'Distinguished Master Guardian', 'Legendary Eagle', 'Legendary Eagle Master',
+      'Supreme Master First Class', 'Global Elite'],
+  },
 });
 
 module.exports = mongoose.model('teams', schema);
