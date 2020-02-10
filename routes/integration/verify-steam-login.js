bc2117399eab745ea698e0eaa118107d966c7cb8
Akseli Kolari <akseli.kolari@tuni.fi>
2020-02-10 15:24:06 +0200

    Some refactoring

--- routes/integration/verify-steam-login.js
+++ routes/integration/verify-steam-login.js
@@ -1,6 +1,7 @@
 const openid = require('openid');
 const config = require('config');
 const { log } = require('../../lib');
+const { User } = require('../../models');
 
 const HOST = config.get('host');
 
@@ -31,24 +32,59 @@ const relyingParty = new openid.RelyingParty(
 const steamIdRegex = new RegExp('^https:\/\/steamcommunity.com\/openid\/id\/([1-9]{17})$');
 
 const handler = async (req, reply) => {
-  relyingParty.verifyAssertion(req.raw.url, (error, result) => {
-    if (error || !result.authenticated) {
-      log.error('Error validating assertion! ', error);
+  let result;
+  try {
+    result = await relyingParty.verifyAssertion(req.raw.url);
+  } catch (error) {
+    log.error('Error validating assertion! ', error);
+    reply.redirect('/integration/steam/login/failed');
+    return;
+  }
+
+  if (!result || !result.authenticated) {
+    reply.redirect('/integration/steam/login/failed');
+    return;
+  }
+
+  let steamId64;
+  try {
+    [, steamId64] = steamIdRegex.exec(result.claimedIdentifier);
+  } catch (err) {
+    log.error('Error matching regex! ', err);
+    reply.redirect('/integration/steam/login/failed');
+    return;
+  }
+
+  let user;
+  try {
+    user = User.findOne({
+      'steam.steamID': steamId64,
+    });
+  } catch (error) {
+    log.error('Error when trying to look for user! ', error);
+    reply.redirect('/integration/steam/login/failed');
+    return;
+  }
+
+  // User already has an account, so just log in and redirect to frontpage
+  if (user) {
+    let token;
+
+    try {
+      token = await reply.jwtSign({ userName: user.userName });
+    } catch (error) {
+      log.error('Error creating token!', error);
       reply.redirect('/integration/steam/login/failed');
       return;
     }
 
-    let steamId64;
-    try {
-      [, steamId64] = steamIdRegex.exec(result.claimedIdentifier);
-    } catch (err) {
-      log.error('Error matching regex! ', err);
-      reply.redirect('/integration/steam/login/failed');
-    }
+    reply.send({ status: 'OK', token }).redirect('/');
+    return;
+  }
 
-
-    reply.redirect(`/integration/steam/login/success/${steamId64}`);
-  });
+  // Start creating account for the user
+  // TODO: Do this :D
+  reply.send({ status: 'OK' }).redirect('/');
 };
 
 
