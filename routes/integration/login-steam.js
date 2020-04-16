b8b33a978e5290e47bee18db474e193eb61c07d2
Akseli Kolari <akseli.kolari@tuni.fi>
2020-04-16 18:04:36 +0300

    Bug fix: Wrong callback for steam

--- routes/integration/login-steam.js
+++ routes/integration/login-steam.js
@@ -3,6 +3,8 @@ const config = require('config');
 const { log } = require('../../lib');
 
 const HOST = config.get('host');
+const ROUTE_PREFIX = config.get('routePrefix');
+
 
 const schema = {
   description: 'Login user with Steam OpenID',
@@ -22,7 +24,7 @@ const schema = {
 
 
 const relyingParty = new openid.RelyingParty(
-  `${HOST}/integration/steam/login/verify`, // Callback url
+  `${HOST}${ROUTE_PREFIX}/integration/steam/login/verify`, // Callback url
   null, // Realm (optional, specifies realm for OpenID authentication)
   true, // Use stateless verification, must be true with Steam
   false, // Strict mode
